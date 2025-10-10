b4dc714554bf5d2e1149093d02bc0e955d24ce0d
Kawan Kakubo <kawanhrs@gmail.com>
2025-10-09 22:24:03 -0300

    feat: add MenuItem and User models, MenuItemRepository, and MenuItemFactory for menu item management

---
+++ api/src/main/java/com/comandei/repository/MenuItemRepository.java
@@ -0,0 +1,10 @@
+package com.comandei.repository;
+
+import com.comandei.models.MenuItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MenuItemRepository extends JpaRepository<MenuItem, Long> {
+
+}
