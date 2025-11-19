ab7cfe8e3f30014e02b904b75883c22f22f4ac0b
Otávio <luizotavio.silva@seidor.com>
2025-11-19 14:04:42 -0300

    feat: configure Spring Boot application to scan additional base packages

--- api/src/main/java/com/comandei/api/ApiApplication.java
+++ api/src/main/java/com/comandei/api/ApiApplication.java
@@ -3,7 +3,7 @@ package com.comandei.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.comandei.api", "com.restaurant.commands"})
 public class ApiApplication {
 
 	public static void main(String[] args) {
