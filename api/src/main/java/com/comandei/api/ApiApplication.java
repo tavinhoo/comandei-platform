1273537fc54dc791532e8a87620fedcc06f349e5
Otávio <luizotavio.silva@seidor.com>
2025-11-26 19:13:19 -0300

    feat: update PostgreSQL version and enhance healthcheck in Docker Compose

--- api/src/main/java/com/comandei/api/ApiApplication.java
+++ api/src/main/java/com/comandei/api/ApiApplication.java
@@ -2,12 +2,18 @@ package com.comandei.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"com.comandei.api", "com.restaurant.commands"})
+@EnableJpaRepositories(basePackages = "com.restaurant.commands.repository")
+@EntityScan(basePackages = "com.restaurant.commands.model")
+@ComponentScan(basePackages = {"com.comandei.api", "com.restaurant.commands"})
 public class ApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
 
 }
